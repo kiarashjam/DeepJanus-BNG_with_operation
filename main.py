960c604f35126a10fb37f44d773f4fe1bd580c9d
kiarashjamshidi <kiarash.jam@gmail.com>
2021-04-12 21:36:18 +0200

    add main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+from self_driving import main_beamng
+list_operation = ["wet_foam", "wet_ripple", "rain", "fog" , "default", "add_obstacle", "changing_illumination", "add_bump"]
+
+if __name__ == '__main__':
+   print("............phase 1.2a ................")
+   main_beamng.start("wet_foam")
