fb4a1b073c0bb8c56b2bde1f80fc91845538d932
kiarashjamshidi <kiarash.jam@gmail.com>
2021-04-12 20:16:57 +0200

    add level

---
+++ levels_template/level_with_rain/tig/art/terrains/materials.cs
@@ -0,0 +1,82 @@
+
+new TerrainMaterial()
+{
+    diffuseMap = "levels/tig/art/terrains/Overlay_Grass-01";
+    detailMap = "levels/tig/art/terrains/Grass-01-D";
+    internalName = "groundmodel_asphalt1";
+    diffuseSize = "150";
+    detailDistance = "50";
+    normalMap = "levels/tig/art/terrains/Grass-01-N";
+    macroSize = "100";
+    macroStrength = "0.4";
+    macroDistance = "450";
+    macroMap = "levels/tig/art/terrains/Macro_grass";
+    detailSize = "3";
+    detailStrength = "0.7";
+    annotation = "GRASS_THAT_BEHAVES_LIKE_ASPHALT";
+};
+
+new TerrainMaterial()
+{
+    diffuseMap = "levels/tig/art/terrains/model_one";
+    detailMap = "levels/tig/art/terrains/model_one_d";
+    internalName = "model_one";
+    diffuseSize = "150";
+    detailDistance = "50";
+    normalMap = "levels/tig/art/terrains/model_one_d_n";
+    macroSize = "100";
+    macroStrength = "0.4";
+    macroDistance = "450";
+    macroMap = "levels/tig/art/terrains/model_one";
+    detailSize = "3";
+    detailStrength = "0.7";
+    annotation = "GRASS_THAT_BEHAVES_LIKE_ASPHALT";
+};
+new TerrainMaterial()
+{
+    diffuseMap = "levels/tig/art/terrains/model_two";
+    detailMap = "levels/tig/art/terrains/model_two_d";
+    internalName = "model_two";
+    diffuseSize = "150";
+    detailDistance = "50";
+    normalMap = "levels/tig/art/terrains/model_two_d_n";
+    macroSize = "100";
+    macroStrength = "0.4";
+    macroDistance = "450";
+    macroMap = "levels/tig/art/terrains/model_two";
+    detailSize = "3";
+    detailStrength = "0.7";
+    annotation = "GRASS_THAT_BEHAVES_LIKE_ASPHALT";
+};
+new TerrainMaterial()
+{
+    diffuseMap = "levels/tig/art/terrains/model_three";
+    detailMap = "levels/tig/art/terrains/model_three_d";
+    internalName = "model_three";
+    diffuseSize = "150";
+    detailDistance = "50";
+    normalMap = "levels/tig/art/terrains/model_three_d_n";
+    macroSize = "100";
+    macroStrength = "0.4";
+    macroDistance = "450";
+    macroMap = "levels/tig/art/terrains/model_three";
+    detailSize = "3";
+    detailStrength = "0.7";
+    annotation = "GRASS_THAT_BEHAVES_LIKE_ASPHALT";
+};
+new TerrainMaterial()
+{
+    diffuseMap = "levels/tig/art/terrains/model_four";
+    detailMap = "levels/tig/art/terrains/model_four_d";
+    internalName = "model_four";
+    diffuseSize = "150";
+    detailDistance = "50";
+    normalMap = "levels/tig/art/terrains/model_four_d_n";
+    macroSize = "100";
+    macroStrength = "0.4";
+    macroDistance = "450";
+    macroMap = "levels/tig/art/terrains/model_four";
+    detailSize = "3";
+    detailStrength = "0.7";
+    annotation = "GRASS_THAT_BEHAVES_LIKE_ASPHALT";
+};
---
+++ levels_template/level_with_wet_floor/tig/art/shapes/signs/sign_chevron_right.cs
@@ -0,0 +1,6 @@
+
+singleton TSShapeConstructor(Sign_chevron_rightDae)
+{
+   baseShape = "./sign_chevron_right.dae";
+   loadLights = "0";
+};
