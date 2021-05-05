4246903ca40bd4924e2caac1e7db269a71442453
kiarashjamshidi <kiarash.jam@gmail.com>
2021-05-05 10:38:14 +0200

    add the threshholds

--- core/config.py
+++ core/config.py
@@ -17,6 +17,24 @@ class Config:
     MUT_OBSTACLE = 'MUT_OBSTACLE'
     MUT_BUMP = 'MUT_BUMP'
 
+    ##### threshold_min
+    FOG_DENSITY_threshold_min = 0
+    WET_FOAM_threshold_min = 0
+    NUMBER_OF_DROP_RAIN_threshold_min = 0
+    WET_RIPPLE_threshold_min = 0
+    NUMBER_BUMP_threshold_min = 0
+    ADDING_OBSTACLE_min = 0
+    ILLUMINATION_AMOUNT_threshold_min = 0
+
+    ##### threshold_max
+    FOG_DENSITY_threshold_max = 1
+    WET_FOAM_threshold_max = 40
+    NUMBER_OF_DROP_RAIN_threshold_max = 30000
+    WET_RIPPLE_threshold_max = 100
+    NUMBER_BUMP_threshold_max = 1000
+    ADDING_OBSTACLE_max = 100
+    ILLUMINATION_AMOUNT_threshold_max = 1
+
 
 
 
@@ -32,7 +50,7 @@ class Config:
         self.MUTATION_EXTENT = 6.0
         self.ARCHIVE_THRESHOLD = 35.0
 
-        self.MUTATION_TYPE = 'MUT_FOG'
+        self.MUTATION_TYPE = Config.MUT_FOG
 
         self.K_SD = 0.01
 
