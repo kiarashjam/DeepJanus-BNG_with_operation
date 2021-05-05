423a14d1d22f87075c92c32fc408becde4af3830
kiarashjamshidi <kiarash.jam@gmail.com>
2021-05-05 10:44:15 +0200

    change the config in the standard way

--- core/config.py
+++ core/config.py
@@ -2,6 +2,12 @@ class Config:
     GEN_RANDOM = 'GEN_RANDOM'
     GEN_RANDOM_SEEDED = 'GEN_RANDOM_SEEDED'
     GEN_SEQUENTIAL_SEEDED = 'GEN_SEQUENTIAL_SEEDED'
+
+    SEG_LENGTH = 25
+    NUM_SPLINE_NODES =10
+    INITIAL_NODE = (0.0, 0.0, -28.0, 8.0)
+    ROAD_BBOX_SIZE = (-250, 0, 250, 500)
+
     MUT_CONTROL_POINTS = 'MUT_CONTROL_POINTS'
     MUT_FOG = 'MUT_FOG'
     MUT_RAIN = 'MUT_RAIN'
@@ -11,11 +17,26 @@ class Config:
     MUT_OBSTACLE = 'MUT_OBSTACLE'
     MUT_BUMP = 'MUT_BUMP'
 
+    ##### threshold_min
+    FOG_DENSITY_threshold_min = 0
+    WET_FOAM_threshold_min = 0
+    NUMBER_OF_DROP_RAIN_threshold_min = 0
+    WET_RIPPLE_threshold_min = 0
+    NUMBER_BUMP_threshold_min = 0
+    ADDING_OBSTACLE_min = 0
+    ILLUMINATION_AMOUNT_threshold_min = 0
 
-    SEG_LENGTH = 25
-    NUM_SPLINE_NODES =10
-    INITIAL_NODE = (0.0, 0.0, -28.0, 8.0)
-    ROAD_BBOX_SIZE = (-250, 0, 250, 500)
+    ##### threshold_max
+    FOG_DENSITY_threshold_max = 1
+    WET_FOAM_threshold_max = 40
+    NUMBER_OF_DROP_RAIN_threshold_max = 30000
+    WET_RIPPLE_threshold_max = 100
+    NUMBER_BUMP_threshold_max = 1000
+    ADDING_OBSTACLE_max = 100
+    ILLUMINATION_AMOUNT_threshold_max = 1
+
+
+
 
     def __init__(self):
         self.experiment_name = 'exp'
@@ -28,6 +49,7 @@ class Config:
 
         self.MUTATION_EXTENT = 6.0
         self.ARCHIVE_THRESHOLD = 35.0
+
         self.MUTATION_TYPE = Config.MUT_CONTROL_POINTS
 
         self.K_SD = 0.01
