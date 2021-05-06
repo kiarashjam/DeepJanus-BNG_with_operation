3541223fd71595847477cb888c5a000b7402bbbc
kiarashjamshidi <kiarash.jam@gmail.com>
2021-05-06 11:40:40 +0200

    adding the wet_floor operation

--- self_driving/beamng_operations.py
+++ self_driving/beamng_operations.py
@@ -17,8 +17,6 @@ default_colorize = [0.4235289990901947, 0.6078429818153381, 0.8666669726371765, 
 default_ambientScale = [0.5450980067253113, 0.5450980067253113, 0.549019992351532, 1]
 default_sunScale = [0.9960780143737793, 0.9019610285758972, 0.8313729763031006, 1]
 
-
-
 ## fog default variables
 default_fog_height = 800
 default_fog_density = 0.0006000000284984708
@@ -30,8 +28,8 @@ default_brightness_sky = 11
 default_time_brightness = 0.1000000014901161
 
 ## wet_floor default variables
-overallRippleMagnitude_default = 500
-overallFoamOpacity_default = 3
+overallRippleMagnitude_default = 0
+overallFoamOpacity_default = 0
 
 
 
@@ -40,14 +38,6 @@ default_drop_size = 0.0799999982
 default_max_Speed =0.4
 default_num_of_drops = 0
 default_max_mass = 0.5
-
-
-
-
-
-# # making the path for generating the
-# path = "testing-" + str(datetime.now().date()) + "-" + str(datetime.now().time().hour) + "-" + str(
-#     datetime.now().time().minute) + "-" + str(datetime.now().time().second)
 
 class MapFolder:
     def __init__(self, path):
@@ -77,29 +67,9 @@ class Maps:
     def __init__(self):
         self.beamng_levels = LevelsFolder(os.path.join(os.environ['USERPROFILE'], r'Documents/BeamNG.research/levels'))
         self.source_levels = LevelsFolder(os.getcwd()+'/levels_template/')
-        # self.source_levels_rain = LevelsFolder(os.getcwd() + '/levels_template/')
-        # self.source_levels_wet_floor = LevelsFolder(os.getcwd() + '/levels_template')
         self.source_map = self.source_levels.get_map('tig/main/MissionGroup/sky_and_sun/')
-        # self.source_map_rain = self.source_levels_rain.get_map('tig')
-        # self.source_map_wet_floor = self.source_levels_wet_floor.get_map('tig')
         self.beamng_map = self.beamng_levels.get_map('tig/main/MissionGroup/sky_and_sun/')
         self.never_logged_path = True
-    # def install_map_with_rain(self):
-    #     if self.never_logged_path:
-    #         self.never_logged_path = False
-    #
-    #     self.beamng_map.delete_all_map()
-    #     shutil.copytree(src=self.source_map_rain.path, dst=self.beamng_map.path)
-    #     print(f'Copying from [{self.source_map_rain.path}] to [{self.beamng_map.path}]')
-    # def install_map_with_wet_floor(self):
-    #     if self.never_logged_path:
-    #         self.never_logged_path = False
-    #
-    #     self.beamng_map.delete_all_map()
-    #     shutil.copytree(src=self.source_map_wet_floor.path, dst=self.beamng_map.path)
-    #     print(f'Copying from [{self.source_map_wet_floor.path}] to [{self.beamng_map.path}]')
-
-
     def install_map(self):
         if self.never_logged_path:
             self.never_logged_path = False
@@ -108,17 +78,6 @@ class Maps:
         shutil.copytree(src=self.source_map.path, dst=self.beamng_map.path)
         print(f'Copying from [{self.source_map.path}] to [{self.beamng_map.path}]')
 
-    # def install_map_river(self):
-    #     print(f'Copying from [{self.source_map_river.path}] to [{self.beamng_map.path}]')
-    #     self.source_map.delete_all_map()
-    #     shutil.copytree(src=self.source_map_river.path, dst=self.source_map.path)
-    # def install_map_default(self):
-    #     print(f'Copying from [{self.source_map_river.path}] to [{self.beamng_map.path}]')
-    #     self.source_map.delete_all_map()
-    #     shutil.copytree(src=self.source_map_default.path, dst=self.source_map.path)
-
-
-
 class Operations:
 
     def default(self):
@@ -126,18 +85,18 @@ class Operations:
 
     def change_fog_amount(self, amount_of_fog):
         default_weather()
-        modification_fog(amount_of_fog)
+        modification_weather(amount_of_fog, "MUT_FOG")
 
     def change_rain_amount(self, amount_of_rain):
         default_weather()
-        modification_rain(amount_of_rain)
+        modification_weather(amount_of_rain, "MUT_RAIN")
 
     def change_foam_amount(self, amount_of_foam):
         default_weather()
-        modification_wet_floor(amount_of_foam, "foam")
+        modification_weather(amount_of_foam, "MUT_WET_FOAM")
     def change_ripple_amount(self, amount_of_ripple):
         default_weather()
-        modification_wet_floor(amount_of_ripple, "ripple")
+        modification_weather(amount_of_ripple, "MUT_WET_RIPPLE")
 
 global operations
 operations = Operations()
@@ -148,13 +107,6 @@ maps = Maps()
 def default_weather():
 
   string_version_of_json=""
-  # if type_level == "rain":
-  #     with open('levels_template/level_with_rain/tig/main/MissionGroup/sky_and_sun/items.level.json') as f:
-  #         list_jasons = f.readlines()
-  # elif type_level == "normal":
-  #     with open('levels_template/main_level/tig/main/MissionGroup/sky_and_sun/items.level.json') as f:
-  #         list_jasons = f.readlines()
-  # elif type_level == "wet_floor":
   with open('levels_template/tig/main/MissionGroup/sky_and_sun/items.level.json') as f:
     list_jasons = f.readlines()
 
@@ -177,6 +129,7 @@ def default_weather():
     elif json_version[i]["class"] == "river":
         json_version[i]["overallFoamOpacity"] = overallFoamOpacity_default
         json_version[i]["overallRippleMagnitude"] = overallRippleMagnitude_default
+        json_version[i]["nodes"] = [[-1000, -1000, -27.9, 1000, 5, 0, 0, 1], [-1000, -1000, -27.9, 1000, 5, 0, 0, 1]]
     elif json_version[i]["class"] == "ScatterSky":
         json_version[i]["shadowSoftness"] = default_shadowSoftness
         json_version[i]["colorize"] = default_colorize
@@ -197,119 +150,69 @@ def default_weather():
   maps.install_map()
 
 
-def modification_fog(fog_density):
-  string_version_of_json = ""
-  with open('levels_template/tig/main/MissionGroup/sky_and_sun/items.level.json') as f:
-    list_jasons = f.readlines()
-  json_version = list()
-  for js in list_jasons:
-    json_version.append(json.loads(js))
-  i = 0
-  while i < len(json_version):
-    if json_version[i]["class"] =="LevelInfo":
-        json_version[i]["fogDensity"] = fog_density
-    elif json_version[i]["class"] == "CloudLayer":
-        json_version[i]["coverage"] = 1
-    elif json_version[i]["class"] == "ScatterSky":
-        json_version[i]["shadowSoftness"] = 1
-        json_version[i]["colorize"] = [0.427451, 0.427451, 0.427451, 1]
-        json_version[i]["ambientScale"] = [0.545098, 0.545098, 0.54902, 1]
-        json_version[i]["sunScale"] = [0.686275, 0.686275, 0.686275, 1]
-        json_version[i]["fogScale"] = [0.756863, 0.760784, 0.760784, 1]
-    i = i + 1
-  i = 0
-  while i < len(json_version):
-    if i == 0:
-      string_version_of_json = str(json.dumps(json_version[i]))
-    else:
-      string_version_of_json = string_version_of_json +"\n"+ str(json.dumps(json_version[i]))
-    i = i + 1
-  writing_json_in_file(string_version_of_json)
-  maps.install_map()
-
-def modification_rain(num_drops):
-    string_version_of_json = ""
-    with open('levels_template/tig/main/MissionGroup/sky_and_sun/items.level.json') as f:
-      list_jasons = f.readlines()
-    json_version = list()
-    for js in list_jasons:
-      json_version.append(json.loads(js))
-    i = 0
-    while i < len(json_version):
-        if json_version[i]["class"] == "LevelInfo":
-            json_version[i]["fogDensity"] = 0.02
-        elif json_version[i]["class"] == "Precipitation":
-            json_version[i]["numDrops"] = num_drops
-        elif json_version[i]["class"] == "CloudLayer":
-            json_version[i]["coverage"] = 1
-        elif json_version[i]["class"] == "ScatterSky":
-            json_version[i]["shadowSoftness"] = 1
-            json_version[i]["colorize"] = [0.427451, 0.427451, 0.427451, 1]
-            json_version[i]["ambientScale"] = [0.545098, 0.545098, 0.54902, 1]
-            json_version[i]["sunScale"] = [0.686275, 0.686275, 0.686275, 1]
-            json_version[i]["fogScale"] = [0.756863, 0.760784, 0.760784, 1]
-        i = i + 1
-    i = 0
-    while i < len(json_version):
-      if i == 0:
-        string_version_of_json = str(json.dumps(json_version[i]))
-      else:
-        string_version_of_json = string_version_of_json + "\n" + str(json.dumps(json_version[i]))
-      i = i + 1
-    writing_json_in_file(string_version_of_json)
-    # maps.install_map_default()
-    maps.install_map()
-
-
-
 
 def writing_json_in_file(string_version_of_json):
     text_file = open('levels_template/tig/main/MissionGroup/sky_and_sun/items.level.json', 'w')
-    # elif type_level == "normal":
-    #     text_file = open('levels_template/main_level/tig/main/MissionGroup/sky_and_sun/items.level.json', 'w')
-    # elif type_level == "wet_floor":
-    #     text_file = open('levels_template/level_with_wet_floor/tig/main/MissionGroup/sky_and_sun/items.level.json', 'w')
     text_file.write(string_version_of_json)
     text_file.close()
 
-# def make_txt_outcome(type,main_value,amount_of_change,modify_value):
-#     with open("outcome/"+path+"/change_weather.txt", 'x') as f:
-#         f.write("changing the weather in type  = " + str(type) + "\n" +
-#                 "main amount of " + str(type) + " is = " + str(main_value) + "\n" +
-#                 "amount of changing is  = " + str(amount_of_change)+"\n" +
-#                 "modify amount of " + str(type) + " is = " + str(modify_value))
+def modification_weather(amount, type_operation):
+    string_version_of_json = ""
+    with open('levels_template/tig/main/MissionGroup/sky_and_sun/items.level.json') as f:
+        list_jasons = f.readlines()
+    json_version = list()
+    for js in list_jasons:
+        json_version.append(json.loads(js))
+    i = 0
+    if type_operation == "MUT_WET_FOAM":
+        while i < len(json_version):
+            if json_version[i]["class"] == "river":
+                json_version[i]["overallFoamOpacity"] = amount
+                json_version[i]["nodes"] = [[-1000, -1000, -27.9, 1000, 5, 0, 0, 1],
+                                            [1000, 1000, -27.9, 1000, 5, 0, 0, 1]]
+            i = i + 1
+    if type_operation == "MUT_WET_RIPPLE":
+        while i < len(json_version):
+            if json_version[i]["class"] == "river":
+                json_version[i]["overallRippleMagnitude"] = amount
+                json_version[i]["nodes"] = [[-1000, -1000, -27.9, 1000, 5, 0, 0, 1],
+                                            [1000, 1000, -27.9, 1000, 5, 0, 0, 1]]
 
-
-
-##### river
-
-
-
-
-def modification_wet_floor(amount,type):
-  string_version_of_json = ""
-  with open('levels_template/tig/main/MissionGroup/sky_and_sun/items.level.json') as f:
-    list_jasons = f.readlines()
-  json_version = list()
-  for js in list_jasons:
-    json_version.append(json.loads(js))
-  i = 0
-  if type == "foam":
-      while i < len(json_version):
-        if json_version[i]["class"] == "river":
-            json_version[i]["overallFoamOpacity"] = amount
+            i = i + 1
+    if type_operation == "MUT_RAIN":
+        while i < len(json_version):
+            if json_version[i]["class"] == "LevelInfo":
+                json_version[i]["fogDensity"] = 0.02
+            elif json_version[i]["class"] == "Precipitation":
+                json_version[i]["numDrops"] = amount
+            elif json_version[i]["class"] == "CloudLayer":
+                json_version[i]["coverage"] = 1
+            elif json_version[i]["class"] == "ScatterSky":
+                json_version[i]["shadowSoftness"] = 1
+                json_version[i]["colorize"] = [0.427451, 0.427451, 0.427451, 1]
+                json_version[i]["ambientScale"] = [0.545098, 0.545098, 0.54902, 1]
+                json_version[i]["sunScale"] = [0.686275, 0.686275, 0.686275, 1]
+                json_version[i]["fogScale"] = [0.756863, 0.760784, 0.760784, 1]
+            i = i + 1
+    if type_operation == "MUT_FOG":
+        while i < len(json_version):
+            if json_version[i]["class"] == "LevelInfo":
+                json_version[i]["fogDensity"] = amount
+            elif json_version[i]["class"] == "CloudLayer":
+                json_version[i]["coverage"] = 1
+            elif json_version[i]["class"] == "ScatterSky":
+                json_version[i]["shadowSoftness"] = 1
+                json_version[i]["colorize"] = [0.427451, 0.427451, 0.427451, 1]
+                json_version[i]["ambientScale"] = [0.545098, 0.545098, 0.54902, 1]
+                json_version[i]["sunScale"] = [0.686275, 0.686275, 0.686275, 1]
+                json_version[i]["fogScale"] = [0.756863, 0.760784, 0.760784, 1]
+            i = i + 1
+    i = 0
+    while i < len(json_version):
+        if i == 0:
+            string_version_of_json = str(json.dumps(json_version[i]))
+        else:
+            string_version_of_json = string_version_of_json +"\n"+ str(json.dumps(json_version[i]))
         i = i + 1
-  if type == "ripple":
-      while i < len(json_version):
-        if json_version[i]["class"] == "river":
-            json_version[i]["overallRippleMagnitude"] = amount
-
-  i = 0
-  while i < len(json_version):
-    if i == 0:
-      string_version_of_json = str(json.dumps(json_version[i]))
-    else:
-      string_version_of_json = string_version_of_json +"\n"+ str(json.dumps(json_version[i]))
-    i = i + 1
-  writing_json_in_file(string_version_of_json,"wet_floor")
-  maps.install_map_with_wet_floor()
+    writing_json_in_file(string_version_of_json)
+    maps.install_map()
