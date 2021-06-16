5f1e4f2cb71d6082470a006eb11036582c04b411
nh17937 <zohdit@usi.ch>
2021-06-16 15:22:43 +0200

    archive changed to Smart

--- self_driving/main_beamng.py
+++ self_driving/main_beamng.py
@@ -9,21 +9,19 @@ sys.path.append(str(path.parent))
 sys.path.append(str(path.parent.parent))
 
 
-from core import nsga2
-from core.archive_impl import GreedyArchive
-from self_driving.beamng_config import BeamNGConfig
-import matplotlib.pyplot as plt
 from self_driving.beamng_problem import BeamNGProblem
+from core import nsga2
+from core.archive_impl import SmartArchive
+from self_driving.beamng_config import BeamNGConfig
+
 
 config = BeamNGConfig()
-problem = BeamNGProblem(config, GreedyArchive())
+
+problem = BeamNGProblem(config, SmartArchive(config.ARCHIVE_THRESHOLD))
 
 if __name__ == '__main__':
     nsga2.main(problem)
     print('done')
-    plt.ioff()
-    plt.show()
-
 
 
     
